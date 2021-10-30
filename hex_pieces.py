516dbe6904b2f4f7d2f52f38dc6166357b797146
Tong Zhang <zhangtong@zerozero.cn>
2021-10-30 18:35:21 +0800

    defined pieces

---
+++ hex_pieces.py
@@ -0,0 +1,141 @@
+import numpy as np
+import libhex
+
+# Number of hex per Hex_Piece
+NUM_OF_HEX_PER_PIECE = 5
+
+
+class Hex_Piece:
+    def __init__(self, form_0, index): 
+        if (len(form_0) != NUM_OF_HEX_PER_PIECE):
+            print('Error: wrong input of hex piece definition (wrong number of hex)')
+            return
+        if str(type(form_0[0])) != "<class 'libhex.Hex'>":
+            print('Error: wrong input of hex piece definition (not formed by hex)')
+            return
+        self.index = index
+        self.form_0 = form_0
+        self.form_1 = []
+        self.form_2 = []
+        self.Calculate_form_1_and_2_from_0()
+        self.color = list(np.random.choice(range(256), size=3)/256.0)
+          
+    def Calculate_form_1_and_2_from_0(self):
+        for hex in self.form_0:
+            self.form_1.append(libhex.hex_rotate_left(libhex.hex_rotate_left(hex)))
+            self.form_2.append(libhex.hex_rotate_right(libhex.hex_rotate_right(hex)))
+
+
+def generate_all_pieces():
+    Puzzle_pieces = []
+
+    piece_0_form_0 = []
+    piece_0_form_0.append(libhex.Hex(0,0,0))
+    piece_0_form_0.append(libhex.Hex(1,0,-1))
+    piece_0_form_0.append(libhex.Hex(0,1,-1))
+    piece_0_form_0.append(libhex.Hex(0,-1,1))
+    piece_0_form_0.append(libhex.Hex(-1,-1,2))
+    Piece_0 = Hex_Piece(piece_0_form_0, 0)
+
+    piece_1_form_0 = []
+    piece_1_form_0.append(libhex.Hex(0,0,0))
+    piece_1_form_0.append(libhex.Hex(0,1,-1))
+    piece_1_form_0.append(libhex.Hex(1,0,-1))
+    piece_1_form_0.append(libhex.Hex(2,0,-2))
+    piece_1_form_0.append(libhex.Hex(0,-1,1))
+    Piece_1 = Hex_Piece(piece_1_form_0, 1)
+
+    piece_2_form_0 = []
+    piece_2_form_0.append(libhex.Hex(0,0,0))
+    piece_2_form_0.append(libhex.Hex(-1,0,1))
+    piece_2_form_0.append(libhex.Hex(1,-1,0))
+    piece_2_form_0.append(libhex.Hex(0,1,-1))
+    piece_2_form_0.append(libhex.Hex(0,2,-2))
+    Piece_2 = Hex_Piece(piece_2_form_0, 2)
+
+    piece_3_form_0 = []
+    piece_3_form_0.append(libhex.Hex(0,0,0))
+    piece_3_form_0.append(libhex.Hex(1,0,-1))
+    piece_3_form_0.append(libhex.Hex(2,0,-2))
+    piece_3_form_0.append(libhex.Hex(0,-1,1))
+    piece_3_form_0.append(libhex.Hex(0,-2,2))
+    Piece_3 = Hex_Piece(piece_3_form_0, 3)
+
+    piece_4_form_0 = []
+    piece_4_form_0.append(libhex.Hex(0,0,0))
+    piece_4_form_0.append(libhex.Hex(0,-1,1))
+    piece_4_form_0.append(libhex.Hex(1,0,-1))
+    piece_4_form_0.append(libhex.Hex(2,0,-2))
+    piece_4_form_0.append(libhex.Hex(3,0,-3))
+    Piece_4 = Hex_Piece(piece_4_form_0, 4)
+
+    piece_5_form_0 = []
+    piece_5_form_0.append(libhex.Hex(0,0,0))
+    piece_5_form_0.append(libhex.Hex(-1,0,1))
+    piece_5_form_0.append(libhex.Hex(0,-1,1))
+    piece_5_form_0.append(libhex.Hex(0,1,-1))
+    piece_5_form_0.append(libhex.Hex(0,2,-2))
+    Piece_5 = Hex_Piece(piece_5_form_0, 5)
+
+    piece_6_form_0 = []
+    piece_6_form_0.append(libhex.Hex(0,0,0))
+    piece_6_form_0.append(libhex.Hex(0,1,-1))
+    piece_6_form_0.append(libhex.Hex(0,-1,1))
+    piece_6_form_0.append(libhex.Hex(-1,0,1))
+    piece_6_form_0.append(libhex.Hex(-2,0,2))
+    Piece_6 = Hex_Piece(piece_6_form_0, 6)
+
+    piece_7_form_0 = []
+    piece_7_form_0.append(libhex.Hex(0,0,0))
+    piece_7_form_0.append(libhex.Hex(0,1,-1))
+    piece_7_form_0.append(libhex.Hex(1,0,-1))
+    piece_7_form_0.append(libhex.Hex(1,-1,0))
+    piece_7_form_0.append(libhex.Hex(1,-2,1))
+    Piece_7 = Hex_Piece(piece_7_form_0, 7)
+
+    piece_8_form_0 = []
+    piece_8_form_0.append(libhex.Hex(0,0,0))
+    piece_8_form_0.append(libhex.Hex(0,1,-1))
+    piece_8_form_0.append(libhex.Hex(0,2,-2))
+    piece_8_form_0.append(libhex.Hex(1,0,-1))
+    piece_8_form_0.append(libhex.Hex(1,-1,0))
+    Piece_8 = Hex_Piece(piece_8_form_0, 8)
+
+    piece_9_form_0 = []
+    piece_9_form_0.append(libhex.Hex(0,0,0))
+    piece_9_form_0.append(libhex.Hex(-1,1,0))
+    piece_9_form_0.append(libhex.Hex(-1,0,1))
+    piece_9_form_0.append(libhex.Hex(1,-1,0))
+    piece_9_form_0.append(libhex.Hex(1,-2,1))
+    Piece_9 = Hex_Piece(piece_9_form_0, 9)
+
+    piece_10_form_0 = []
+    piece_10_form_0.append(libhex.Hex(0,0,0))
+    piece_10_form_0.append(libhex.Hex(-1,0,1))
+    piece_10_form_0.append(libhex.Hex(0,-1,1))
+    piece_10_form_0.append(libhex.Hex(0,1,-1))
+    piece_10_form_0.append(libhex.Hex(1,1,-2))
+    Piece_10 = Hex_Piece(piece_10_form_0, 10)
+
+    piece_11_form_0 = []
+    piece_11_form_0.append(libhex.Hex(0,0,0))
+    piece_11_form_0.append(libhex.Hex(0,1,-1))
+    piece_11_form_0.append(libhex.Hex(0,2,-2))
+    piece_11_form_0.append(libhex.Hex(1,-1,0))
+    piece_11_form_0.append(libhex.Hex(1,-2,1))
+    Piece_11 = Hex_Piece(piece_11_form_0, 11)
+
+    Puzzle_pieces.append(Piece_0)
+    Puzzle_pieces.append(Piece_1)
+    Puzzle_pieces.append(Piece_2)
+    Puzzle_pieces.append(Piece_3)
+    Puzzle_pieces.append(Piece_4)
+    Puzzle_pieces.append(Piece_5)
+    Puzzle_pieces.append(Piece_6)
+    Puzzle_pieces.append(Piece_7)
+    Puzzle_pieces.append(Piece_8)
+    Puzzle_pieces.append(Piece_9)
+    Puzzle_pieces.append(Piece_10)
+    Puzzle_pieces.append(Piece_11)
+
+    return Puzzle_pieces
--- hexagon_puzzle.py
+++ hexagon_puzzle.py
@@ -3,7 +3,11 @@ from matplotlib.patches import RegularPolygon
 import numpy as np
 from typing import NamedTuple
 
-import libhex
+import libhex, hex_pieces
+
+
+## Settings
+SIZE_MAP = 4   # Map size
 
 # Hex_Grid class. Contains hex position, and grid value. 
 # Grid value:
@@ -14,14 +18,25 @@ class Hex_Grid(NamedTuple):
     value: int
 
 # Generate hex map
-N = 4
-Hex_Map = []
-for x in range(-N, N+1):
-    for y in range(max(-N, -x-N), min(+N, -x+N)+1):
-        z = -x-y
-        this_grid = Hex_Grid(libhex.Hex(x, y, z), 0)
-        Hex_Map.append(this_grid)
-#print(Hex_Map)
+def Hex_generate_map(map_size):
+    Map = []
+    for x in range(-map_size, map_size+1):
+        for y in range(max(-map_size, -x-map_size), min(+map_size, -x+map_size)+1):
+            z = -x-y
+            this_grid = Hex_Grid(libhex.Hex(x, y, z), 0)
+            Map.append(this_grid)
+    return Map
+
+
+## Set problem
+Hex_Map = Hex_generate_map(SIZE_MAP)
+
+# Place problem definition grid (single cell with pin)
+pin_hex = libhex.Hex(0, 0, 0)
+
+# Generate puzzle pieces
+Puzzle_pieces = hex_pieces.generate_all_pieces()
+
 
 # Plot the map
 fig, ax = plt.subplots(1)
@@ -35,6 +50,16 @@ for grid in Hex_Map:
                          orientation=np.radians(30), 
                          facecolor='orange', alpha=0.2, edgecolor='k')
     ax.add_patch(hex)
+
+for piece in Puzzle_pieces:
+    piece_form = piece.form_0
+    for hex in piece_form:
+        x = hex.q
+        y = 2. * np.sin(np.radians(60)) * (hex.r - hex.s) /3.
+        hex = RegularPolygon((x, y), numVertices=6, radius=2. / 3., 
+                            orientation=np.radians(30), 
+                            facecolor=piece.color, alpha=0.3, edgecolor='k')
+        ax.add_patch(hex)
 
 ax.autoscale_view()
 plt.axis('off')
